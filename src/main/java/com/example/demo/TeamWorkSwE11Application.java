86744dc547ba8dc59a1787c1c83e26de802047e6
Asier Gómez Viteri <asier.gomez@opendeusto.es>
2025-10-31 08:36:57 +0100

    prueba

--- src/main/java/com/example/demo/TeamWorkSwE11Application.java
+++ src/main/java/com/example/demo/TeamWorkSwE11Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamWorkSwE11Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TeamWorkSwE11Application.class, args); //commit sam
+		SpringApplication.run(TeamWorkSwE11Application.class, args); //commit asier
 	}
 
 }
